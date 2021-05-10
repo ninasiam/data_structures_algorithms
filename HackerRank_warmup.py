cc2c6fb8ede0f3c3cb3618d2d946fdfaba5f28a4
Nina Siaminou <nina.siaminou@gmail.com>
2021-05-10 19:28:26 +0300

    add: more problems on arrays

--- HackerRank_warmup.py
+++ HackerRank_warmup.py
@@ -79,6 +79,64 @@ def rotLeft2(a, d):
             a_rot.append(popped)
     return a_rot  
 
+def minimumBribes(q):
+    # Write your code here
+    len_q = len(q)
+    bribes = 0
+    cut_liners = 0
+    if len_q > 0:
+        for j in range(len_q):
+            jumps = q[j] - j 
+            if jumps - 1 > 2:
+                print("Too chaotic")
+                return
+            # count how many bribed the person in position j
+            for i in range(0, j):
+                if q[j] < q[i]:
+                    bribes += 1
+        print(bribes)
+
+def minimumSwaps(arr):
+    min_swaps = 0
+    for i, v in enumerate(arr):
+        if i == v - 1:
+            continue
+        else:
+            arr[v - 1], arr[i] = v, arr[v - 1]
+            min_swaps += 1
+    print(arr)
+    return min_swaps
+    
+# This solution timed out    
+def arrayManipulation(n, queries):
+    # number of queries
+    n_queries = len(queries)
+    max_element = 0
+    my_arr = (n + 1) * [0]
+    # for each query (operation)
+    for query in queries:
+        my_arr[query[0] - 1: query[1] + 1] = map(lambda x: x + query[2], my_arr[query[0]:query[1] + 1]) 
+    max_element = max(my_arr) 
+    return max_element
+        
+
+def arrayManipulation(n, queries):
+    # number of queries
+    n_queries = len(queries)
+    max_element = 0
+    sum_so_far = 0
+    my_arr = (n + 1) * [0]
+    # for each query (operation)
+    for query in queries:
+        # Since in the specified intervals 
+        # the values are the same we keep only the bounds
+        my_arr[query[0] - 1] += query[2]
+        my_arr[query[1]] -= query[2]
+        
+    for e in my_arr:
+        sum_so_far += e
+        max_element = max(max_element, sum_so_far)
+    return max_element
 #---------------  Testing -----------------
 # Warm up
 steps = jumping_on_clouds([0,0,1,0,0,1,0])
@@ -104,3 +162,22 @@ print(max_sum)
 a_2 = [1, 2, 3, 4, 5]
 a_2 = rotLeft2(a_2, 4)
 print(a_2)
+
+q = [2, 1, 5, 3, 4]
+minimumBribes(q)
+
+q = [2, 5, 1, 3, 4]
+minimumBribes(q)
+
+q = [1, 2, 5, 3, 4, 7, 8, 6]
+minimumBribes(q)
+
+q = [5, 1, 2, 3, 7, 8, 6, 4]
+minimumBribes(q)
+
+q = [1, 2, 5, 3, 7, 8, 6, 4]
+minimumBribes(q)
+
+arr = [4, 3, 1, 2]
+min_swaps = minimumSwaps(arr)
+print(min_swaps)
